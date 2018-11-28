536b63c956adafcdce21724c0a340da8b16c1ce5
Alan Daniel <adaniel@grupoasesores.com.mx>
2018-11-28 10:29:49 -0600

    primer comit

---
+++ precache-manifest.7d99ce75b2f6790ab22fb7527b8027ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb05230068bcfcac9e4a",
+    "url": "/static/js/main.cb052300.chunk.js"
+  },
+  {
+    "revision": "3daf17738b6a3d7c0929",
+    "url": "/static/js/1.3daf1773.chunk.js"
+  },
+  {
+    "revision": "cb05230068bcfcac9e4a",
+    "url": "/static/css/main.566e72e0.chunk.css"
+  },
+  {
+    "revision": "f43032c8187df76c625b1b6f19231656",
+    "url": "/index.html"
+  }
+];
