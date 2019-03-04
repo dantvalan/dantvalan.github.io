c5c6adc51a8bfc97817e329fe86670630eee9094
Alan Daniel <adaniel@grupoasesores.com.mx>
2019-03-04 12:33:42 -0600

    update

---
+++ precache-manifest.660ce43f51123cf0ae8cb9303ad374c8.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a07ff3faccdc86b07f701ea8224dc97",
+    "url": "/static/media/Balanza.8a07ff3f.svg"
+  },
+  {
+    "revision": "df8fbeb0b5df1dbab369",
+    "url": "/static/css/main.4b8dfcea.chunk.css"
+  },
+  {
+    "revision": "de5af659ac99de6bf121",
+    "url": "/static/js/1.de5af659.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "949e0e8c6d8d5acfdfc9a57dc134bfb6",
+    "url": "/static/media/ico-terminar.949e0e8c.svg"
+  },
+  {
+    "revision": "d3529177762bbabf3741b6a3667274e8",
+    "url": "/static/media/Base.d3529177.svg"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "a83da698ba4ab615b6084f434e998bf9",
+    "url": "/static/media/Personaje_balanza.a83da698.png"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "401c7200b3ec03520a6b0e6d744ab865",
+    "url": "/static/media/fondo-instrucciones.401c7200.png"
+  },
+  {
+    "revision": "05289e866fe7e1e99d27a7a31f8d3b66",
+    "url": "/static/media/Trattatello.05289e86.ttf"
+  },
+  {
+    "revision": "63641e03bee0358dd351984df2c69140",
+    "url": "/static/media/Repisa.63641e03.svg"
+  },
+  {
+    "revision": "5e442a446bbc541e6db7014215d6cb50",
+    "url": "/static/media/Fondo.5e442a44.jpg"
+  },
+  {
+    "revision": "df8fbeb0b5df1dbab369",
+    "url": "/static/js/main.df8fbeb0.chunk.js"
+  },
+  {
+    "revision": "aaf4a751ada0a46db9a8810a644ad44f",
+    "url": "/static/media/ico-descarga.aaf4a751.svg"
+  },
+  {
+    "revision": "f8c8563d29edc9ea73acd8b370eef118",
+    "url": "/static/media/eraser.f8c8563d.svg"
+  },
+  {
+    "revision": "26a99a244b4bdf9e9d9b1317fdcb124f",
+    "url": "/static/media/pencil.26a99a24.svg"
+  },
+  {
+    "revision": "75bd9a89d63a372c702619efbb96114a",
+    "url": "/static/media/text.75bd9a89.svg"
+  },
+  {
+    "revision": "ec0bd0c91faab9de1b1b12f03646d362",
+    "url": "/static/media/line.ec0bd0c9.svg"
+  },
+  {
+    "revision": "46cc242b8e91b519ea6af08cb4347b80",
+    "url": "/static/media/reset.46cc242b.svg"
+  },
+  {
+    "revision": "5eaf37e9d6ba1eb4ce03ec1348777561",
+    "url": "/static/media/arrow.5eaf37e9.svg"
+  },
+  {
+    "revision": "48fb131631b2204a55dd3a13bb0325c2",
+    "url": "/static/media/bucket.48fb1316.svg"
+  },
+  {
+    "revision": "ae708d68194aa175518d81d2818c3ed6",
+    "url": "/static/media/square.ae708d68.svg"
+  },
+  {
+    "revision": "5af5d61ee4fea74e1cad86c117fd0e08",
+    "url": "/static/media/circle.5af5d61e.svg"
+  },
+  {
+    "revision": "9496470b55c19cdfcf28b4c397bf8460",
+    "url": "/static/media/fondo_canvas.9496470b.svg"
+  },
+  {
+    "revision": "849cd51aa73a574a88d678566affc6bf",
+    "url": "/static/media/triangle.849cd51a.svg"
+  },
+  {
+    "revision": "372451a3688fbaf65ed8282b1414c440",
+    "url": "/static/media/fondo-herramientas.372451a3.svg"
+  },
+  {
+    "revision": "ceeb616aa5ba3d887be3bfec83532c02",
+    "url": "/index.html"
+  }
+];
